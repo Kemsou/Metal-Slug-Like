e015c6d10208f88c623b1be8a24c662c7898a00a
Patalano <clement.patalano@gmail.com>
2017-10-19 16:55:55 +0200

    Clement's level part

---
+++ Assets/Niveau_Clement/tilemap.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_spritesheet" tilewidth="70" tileheight="70" spacing="2" tilecount="156" columns="12">
+ <image source="D:/Tiled/MesTiles/tiles_spritesheet.png" trans="000000" width="914" height="936"/>
+ <tile id="30">
+  <objectgroup draworder="index">
+   <object id="1" x="-4" y="-192" width="1" height="1"/>
+   <object id="5" x="0" y="0">
+    <polygon points="0,0 70,70 0,70"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index">
+   <object id="3" x="70" y="0">
+    <polygon points="0,0 -70,70 0,70"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="70" height="70"/>
+  </objectgroup>
+ </tile>
+ <tile id="121">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="70" height="70"/>
+  </objectgroup>
+ </tile>
+ <tile id="133">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="70" height="70"/>
+  </objectgroup>
+ </tile>
+ <tile id="154">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="70" height="70"/>
+  </objectgroup>
+ </tile>
+</tileset>
