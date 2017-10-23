c50296200b24cdb0d507571a5c2d6735e89abc58
Unknown <maixuanhien.vn93@gmail.com>
2017-10-23 15:46:57 +0200

    level 1

--- maixuanhien_test/Assets/Scripts/cameraFollow.cs
+++ maixuanhien_test/Assets/Scripts/cameraFollow.cs
@@ -11,23 +11,20 @@ public class cameraFollow : MonoBehaviour {
     float minY;
     float offset;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         minX = transform.position.x;
         offset = transform.position.y - target.position.y;
         minY = -12;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     private void FixedUpdate() {
-        if(target.position.x >= minX && target.position.y >= minY) {
-            Vector3 targetCameraPosition = new Vector3(target.position.x, target.position.y+2, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, targetCameraPosition, smoothing * Time.deltaTime);
-        }
-        
+        Vector3 targetCameraPosition = new Vector3(target.position.x, target.position.y + 2, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetCameraPosition, smoothing * Time.deltaTime);
     }
 }
