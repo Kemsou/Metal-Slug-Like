5516950628983a732437d53437cf0d436079fcb3
gwengomez <eponia22@gmail.com>
2017-10-23 10:57:14 +0200

    Fix : direction of projectile and laser beam (8 directions)

--- maixuanhien_test/Assets/Scripts/ProjectileControler.cs
+++ maixuanhien_test/Assets/Scripts/ProjectileControler.cs
@@ -7,21 +7,13 @@ public class ProjectileControler : MonoBehaviour {
     public float bulletSpeed;
 
     Rigidbody2D bulletBody;
-
-    //awake goi truoc start
-    private void Awake() {
-        bulletBody = GetComponent<Rigidbody2D>();
-        if (transform.localRotation.z > 0) {
-            bulletBody.AddForce(new Vector2(-1, 0) * bulletSpeed, ForceMode2D.Impulse);
-        } else {
-            bulletBody.AddForce(new Vector2(1, 0) * bulletSpeed, ForceMode2D.Impulse);
-        }
-    }
+    
 
     // Use this for initialization
     void Start () {
-		
-	}
+        bulletBody = GetComponent<Rigidbody2D>();
+        bulletBody.AddForce(this.transform.right * bulletSpeed, ForceMode2D.Impulse);
+    }
 	
 	// Update is called once per frame
 	void Update () {
