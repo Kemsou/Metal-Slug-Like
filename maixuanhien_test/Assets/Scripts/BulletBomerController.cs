647951459603302769827067713ea82bd43c9021
gwengomez <eponia22@gmail.com>
2017-10-24 13:51:49 +0200

    Last issues resolved

--- maixuanhien_test/Assets/Scripts/BulletBomerController.cs
+++ maixuanhien_test/Assets/Scripts/BulletBomerController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,31 +24,3 @@ public class BulletBomerController : MonoBehaviour {
         bulletBody.velocity = new Vector2(0, 0);
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BulletBomerController : MonoBehaviour {
-
-    [SerializeField]
-    float bulletSpeed;
-
-    Rigidbody2D bulletBody;
-
-	// Use this for initialization
-	void Start () {
-        bulletBody = GetComponent<Rigidbody2D>();
-        bulletBody.AddForce(this.transform.right * bulletSpeed, ForceMode2D.Impulse);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void removeForce() {
-        bulletBody.velocity = new Vector2(0, 0);
-    }
-}
->>>>>>> 8e32229531b2e655db7149cf7efa9380102e4345
--- maixuanhien_test/Assets/Scripts/BulletBomerHit.cs
+++ maixuanhien_test/Assets/Scripts/BulletBomerHit.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,25 +18,3 @@ public class BulletBomerHit : MonoBehaviour {
         }
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BulletBomerHit : MonoBehaviour {
-
-    [SerializeField]
-    GameObject bulletBomerExplosion;
-    [SerializeField]
-    float damage;
-
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.tag == "Player") {
-            Instantiate(bulletBomerExplosion, transform.position, transform.rotation);
-            Destroy(gameObject);
-            PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
-            player.addDamage(damage);
-        }
-    }
-}
->>>>>>> 8e32229531b2e655db7149cf7efa9380102e4345
