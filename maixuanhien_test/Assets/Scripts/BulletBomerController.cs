cb2e95a009f22d9c278e7fb36acae3acf0484aa5
Unknown <maixuanhien.vn93@gmail.com>
2017-10-24 13:12:47 +0200

    modifier enemy bullet

--- maixuanhien_test/Assets/Scripts/BulletBomerController.cs
+++ maixuanhien_test/Assets/Scripts/BulletBomerController.cs
@@ -2,26 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileControler : MonoBehaviour {
+public class BulletBomerController : MonoBehaviour {
 
     [SerializeField]
     float bulletSpeed;
-    Rigidbody2D bulletBody;
-    
 
-    // Use this for initialization
-    void Start () {
+    Rigidbody2D bulletBody;
+
+	// Use this for initialization
+	void Start () {
         bulletBody = GetComponent<Rigidbody2D>();
         bulletBody.AddForce(this.transform.right * bulletSpeed, ForceMode2D.Impulse);
-    }
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    //chuc nang lam vien dan dung lai
-    public void removeForce() {
+    void removeForce() {
         bulletBody.velocity = new Vector2(0, 0);
     }
 }
