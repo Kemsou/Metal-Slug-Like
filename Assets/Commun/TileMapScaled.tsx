a126b2ad62c53707756915acd6d64daf922df67f
Elric <emathure@gmail.com>
2017-10-24 14:05:23 +0200

    Colliders refaient (en espérant que ça soit la dernière putain de fois)

--- Assets/Commun/TileMapScaled.tsx
+++ Assets/Commun/TileMapScaled.tsx
@@ -1,6 +1,5 @@
-<<<<<<< HEAD:Assets/Commun/TileMapScaled.tsx
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="spritesheetModifie" tilewidth="70" tileheight="70" spacing="2" tilecount="156" columns="12">
+<tileset name="spritesheetModifie - Scaled" tilewidth="70" tileheight="70" spacing="2" tilecount="156" columns="12">
  <image source="spritesheetModifie - Scaled.png" width="914" height="936"/>
  <tile id="22">
   <objectgroup draworder="index">
@@ -10,15 +9,9 @@
  <tile id="23">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
   </objectgroup>
  </tile>
  <tile id="32">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="33">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="70" height="70"/>
   </objectgroup>
@@ -41,7 +34,11 @@
  <tile id="47">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="70" height="70"/>
   </objectgroup>
  </tile>
  <tile id="58">
@@ -52,12 +49,11 @@
  <tile id="70">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
   </objectgroup>
  </tile>
  <tile id="71">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="39"/>
+   <object id="1" x="0" y="0" width="70" height="40.6667"/>
   </objectgroup>
  </tile>
  <tile id="82">
@@ -77,33 +73,31 @@
  </tile>
  <tile id="95">
   <objectgroup draworder="index">
-   <object id="3" x="0" y="70">
-    <polygon points="0,0 70,0 0,-70"/>
+   <object id="1" x="0" y="70">
+    <polygon points="0,0 0,-70 70,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="105">
   <objectgroup draworder="index">
-   <object id="1" x="69.3334" y="70" rotation="-90">
-    <polygon points="0,0 -0.333333,-69.6667 69.6667,0.333333"/>
+   <object id="1" x="70" y="70">
+    <polygon points="0,0 0,-70 -70,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="106">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
   </objectgroup>
  </tile>
  <tile id="107">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
   </objectgroup>
  </tile>
  <tile id="117">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="40"/>
+   <object id="1" x="0" y="0" width="70" height="40.9091"/>
   </objectgroup>
  </tile>
  <tile id="118">
@@ -113,17 +107,27 @@
  </tile>
  <tile id="119">
   <objectgroup draworder="index">
-   <object id="1" x="0.25" y="0.25" width="69.75" height="39.5"/>
+   <object id="1" x="0" y="0" width="70" height="40.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="126">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="70" height="39.8182"/>
   </objectgroup>
  </tile>
  <tile id="130">
   <objectgroup draworder="index">
-   <object id="1" x="-0.25" y="0" width="70.5" height="70"/>
+   <object id="1" x="0" y="0" width="70" height="70"/>
+  </objectgroup>
+ </tile>
+ <tile id="136">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="70" height="70"/>
   </objectgroup>
  </tile>
  <tile id="142">
   <objectgroup draworder="index">
-   <object id="1" x="-0.25" y="0" width="70" height="70"/>
+   <object id="1" x="0" y="0" width="70" height="70"/>
   </objectgroup>
  </tile>
  <tile id="154">
@@ -132,138 +136,3 @@
   </objectgroup>
  </tile>
 </tileset>
-=======
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="spritesheetModifie" tilewidth="70" tileheight="70" spacing="2" tilecount="156" columns="12">
- <image source="spritesheetModifie.png" width="914" height="936"/>
- <tile id="22">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="23">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="32">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="33">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="34">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="35">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="46">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="47">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="58">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="70">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="71">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="39"/>
-  </objectgroup>
- </tile>
- <tile id="82">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="83">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="94">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="95">
-  <objectgroup draworder="index">
-   <object id="3" x="0" y="70">
-    <polygon points="0,0 70,0 0,-70"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="105">
-  <objectgroup draworder="index">
-   <object id="1" x="69.3334" y="70" rotation="-90">
-    <polygon points="0,0 -0.333333,-69.6667 69.6667,0.333333"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="106">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="107">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-   <object id="2" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="117">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="40"/>
-  </objectgroup>
- </tile>
- <tile id="118">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="119">
-  <objectgroup draworder="index">
-   <object id="1" x="0.25" y="0.25" width="69.75" height="39.5"/>
-  </objectgroup>
- </tile>
- <tile id="130">
-  <objectgroup draworder="index">
-   <object id="1" x="-0.25" y="0" width="70.5" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="142">
-  <objectgroup draworder="index">
-   <object id="1" x="-0.25" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
- <tile id="154">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="70" height="70"/>
-  </objectgroup>
- </tile>
-</tileset>
->>>>>>> master:Assets/Commun/TileMapModifie.tsx
