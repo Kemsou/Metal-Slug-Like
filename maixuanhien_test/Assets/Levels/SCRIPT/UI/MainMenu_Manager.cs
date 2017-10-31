ee0fb03af6070cb87a1c24d785d7fedb8c95a4f8
Unknown <maixuanhien.vn93@gmail.com>
2017-10-31 10:02:29 +0100

    add main menu

---
+++ maixuanhien_test/Assets/Levels/SCRIPT/UI/MainMenu_Manager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_Manager : MonoBehaviour {
+	
+
+	public void GameStart(string scenename)
+	{
+		//mettre dans le string le nom de la scene du level à charger
+		SceneManager.LoadScene("Level Elric");
+	}
+}
